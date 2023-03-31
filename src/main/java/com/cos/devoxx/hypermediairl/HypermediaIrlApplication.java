04a5a60b8c58e61bca2d88e21735f03514cf04e1
hugothomas <hugo.thomas98@gmail.com>
2023-03-31 11:59:18 +0200

    initialize spring boot project

---
+++ src/main/java/com/cos/devoxx/hypermediairl/HypermediaIrlApplication.java
@@ -0,0 +1,13 @@
+package com.cos.devoxx.hypermediairl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypermediaIrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HypermediaIrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cos/devoxx/hypermediairl/HypermediaIrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.devoxx.hypermediairl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypermediaIrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
