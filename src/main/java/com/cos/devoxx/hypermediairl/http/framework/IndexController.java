d076ff2f75c4405eca329fade58ec65d9826227f
rgarcia <rgarcia@cosium.com>
2023-04-07 11:03:55 +0200

    Refactored IndexController

--- src/main/java/com/cos/devoxx/hypermediairl/http/framework/IndexController.java
+++ src/main/java/com/cos/devoxx/hypermediairl/http/framework/IndexController.java
@@ -5,9 +5,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import com.cos.devoxx.hypermediairl.http.item.ItemController;
 import com.cos.devoxx.hypermediairl.http.spectacles.SpectaclesController;
-import java.util.ArrayList;
 import java.util.List;
-import org.springframework.hateoas.Link;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilderFactory;
 import org.springframework.http.ResponseEntity;
@@ -15,22 +13,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-  @Controller
-  @RequestMapping("/")
-  public class IndexController {
+@Controller
+@RequestMapping("/")
+public class IndexController {
 
-    private final WebMvcLinkBuilderFactory linkBuilders;
+  private final WebMvcLinkBuilderFactory linkBuilders;
 
-    public IndexController(WebMvcLinkBuilderFactory linkBuilders) {
-      this.linkBuilders = requireNonNull(linkBuilders);
-    }
-
-    @GetMapping
-    public ResponseEntity<RepresentationModel<?>> get() {
-      List<Link> links = new ArrayList<>();
-      links.add(linkBuilders.linkTo(methodOn(ItemController.class).list(null)).withRel("items"));
-      links.add(
-          linkBuilders.linkTo(methodOn(SpectaclesController.class).list(null)).withRel("spectacles"));
-      return ResponseEntity.ok(new RepresentationModel<>(links));
-    }
+  public IndexController(WebMvcLinkBuilderFactory linkBuilders) {
+    this.linkBuilders = requireNonNull(linkBuilders);
   }
+
+  @GetMapping
+  public ResponseEntity<RepresentationModel<?>> get() {
+    return ResponseEntity.ok(
+        new RepresentationModel<>(
+            List.of(
+                linkBuilders.linkTo(methodOn(ItemController.class).list(null)).withRel("items"),
+                linkBuilders
+                    .linkTo(methodOn(SpectaclesController.class).list(null))
+                    .withRel("spectacles"))));
+  }
+}
